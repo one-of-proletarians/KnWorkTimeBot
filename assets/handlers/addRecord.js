b852a57276002af737e683129352f4e665dfc0a2
Vitalii <0x1d46@gmail.com>
2022-03-15 14:34:20 +0100

    Перенес обратно из middleware

--- assets/handlers/addRecord.js
+++ assets/handlers/addRecord.js
@@ -1,9 +1,19 @@
+import { stringToDate, strToArr } from "../helpers.js";
 import { saveOrUpdate } from "./saveOrUpdate.js";
 
 export default function useAddRecord() {
   return (ctx) => {
-    const { date, period } = ctx.session.state;
-    if (date.error) return ctx.reply("Не корректная дата");
-    saveOrUpdate(ctx, period, date.input);
+    const onlyTime =
+      ctx.message.text?.replace(/\D/g, "-").split("-").length === 2;
+
+    let text = ctx.message.text.replace(/\s+/g, " ");
+    let rawDate = "-";
+
+    if (!onlyTime) [rawDate, text] = text.replace(/\s/, "*").split("*");
+    const date = stringToDate(rawDate);
+    const period = strToArr(text.replace(" ", "-"));
+
+    if (!date && !onlyTime) return ctx.reply("Не корректная дата");
+    saveOrUpdate(ctx, period, date);
   };
 }
