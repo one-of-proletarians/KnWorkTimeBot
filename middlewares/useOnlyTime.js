6d766eefa3d65df9ced2fa39cd432c28d0f96d4d
Vitalii <0x1d46@gmail.com>
2022-03-15 14:33:19 +0100

    Middleware срабатывает до обрботчика сообщений

--- middlewares/useOnlyTime.js
+++
@@ -1,7 +0,0 @@
-export default () => (ctx, next) => {
-  if (!ctx.message) return next();
-  const onlyTime =
-    ctx.message.text?.replace(/\D/g, "-").split("-").length === 2;
-  ctx.session.state.onlyTime = onlyTime;
-  next();
-};
