65c5ec4a6c1a1faa81b9e27a59722aca073d3f8b
Vitalii <0x1d46@gmail.com>
2022-03-15 16:40:57 +0100

    Вынос функции печати таблицы

--- assets/commands/infoCommand.js
+++ assets/commands/infoCommand.js
@@ -1,12 +1,5 @@
-import Table from "easy-table";
-import months from "../months.js";
 import { User } from "../../mongo/mongo.js";
-
-function getWeekDay(date) {
-  const [day, month, year] = date.split(".").map((e) => +e);
-  const days = ["Вс", "Пн", "Вт", "Ср", "Чт", "Пт", "Сб"];
-  return days[new Date(year, month - 1, day).getDay()];
-}
+import { printTable } from "../printTable.js";
 
 export default function useInfoCommand() {
   return async (ctx) => {
@@ -16,45 +9,15 @@ export default function useInfoCommand() {
     });
 
     const _id = ctx.message.from.id;
-    const reg = new RegExp(`\\d{2}\\.${date}`);
 
     const user = await User.findOne({
       _id,
       records: {
-        $elemMatch: {
-          date: {
-            $regex: new RegExp(`\\d{2}\\.${date}`),
-          },
-        },
+        $elemMatch: { date: { $regex: new RegExp(`\\d{2}\\.${date}`) } },
       },
     });
 
-    if (user) {
-      const [month, year] = date.split(".").map((e) => +e);
-      const t = new Table();
-
-      user.records.forEach((e) => {
-        if (!reg.test(e.date)) return;
-        const { start, end } = e.value;
-
-        const period = `${start}:00 - ${end}:00`.replace("7", " 7");
-
-        t.cell("День", getWeekDay(e.date));
-        t.cell("Дата", +e.date.substring(0, 2));
-        t.cell("Смена", period);
-        t.cell("Часы", e.value.amount, Table.number());
-        t.newRow();
-      });
-
-      t.sort(["Дата|asc"]);
-      t.total("Дата", {
-        printer: (val) => Table.padLeft(val, 0),
-        reduce: (acc) => acc + 1,
-      });
-      t.total("Часы");
-
-      await ctx.replyWithHTML(`<b><i>${year} ${months[month - 1]}</i></b>`);
-      ctx.replyWithHTML(`<pre>${t}</pre>`);
-    } else ctx.reply("За текущий месяц записи отсутствуют.");
+    if (user) printTable(ctx, date, user.records);
+    else ctx.reply("За текущий месяц записи отсутствуют.");
   };
 }
---
+++ assets/printTable.js
@@ -0,0 +1,48 @@
+import Table from "easy-table";
+import months from "./months.js";
+
+/**
+ *
+ * @param {Telegraf<Context>} ctx Контекст Телеграфа
+ * @param {Date} date Дата в виде строки
+ * @param {Array<UserRecord>} records Массив записей
+ */
+export const printTable = async (ctx, date, records) => {
+  const [month, year] = date.split(".").map((e) => +e);
+  const reg = new RegExp(`\\d{2}\\.${date}`);
+  const t = new Table();
+
+  records.forEach((e) => {
+    if (!reg.test(e.date)) return;
+    const { start, end } = e.value;
+
+    const period = `${start}:00 - ${end}:00`.replace("7", " 7");
+
+    t.cell("День", getWeekDay(e.date));
+    t.cell("Дата", +e.date.substring(0, 2));
+    t.cell("Смена", period);
+    t.cell("Часы", e.value.amount, Table.number());
+    t.newRow();
+  });
+
+  t.sort(["Дата|asc"]);
+  t.total("Дата", {
+    printer: (val) => Table.padLeft(val, 0),
+    reduce: (acc) => acc + 1,
+  });
+  t.total("Часы");
+
+  await ctx.replyWithHTML(`<b><i>${year} ${months[month - 1]}</i></b>`);
+  ctx.replyWithHTML(`<pre>${t}</pre>`);
+};
+
+/**
+ *
+ * @param {Date} date
+ * @returns День недели по имени соответствующий дате
+ */
+function getWeekDay(date) {
+  const [day, month, year] = date.split(".").map((e) => +e);
+  const days = ["Вс", "Пн", "Вт", "Ср", "Чт", "Пт", "Сб"];
+  return days[new Date(year, month - 1, day).getDay()];
+}
