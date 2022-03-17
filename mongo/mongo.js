b6db1491eb768ff5c65bfa6576a0aa7487291d8d
Vitalii <0x1d46@gmail.com>
2022-03-17 15:44:59 +0100

    prettier

--- mongo/mongo.js
+++ mongo/mongo.js
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
 import { isDev } from "../assets/helpers.js";
-import UserSchema from "./schemas/UserSchema.js";
+import { UserScheme } from "./schemas/UserSchema.js";
 
 const dev = isDev();
 const mongoConnect = process.env[dev ? "DEV_MONGO" : "MONGO"];
 
 const db = mongoose.createConnection(mongoConnect);
 
-export const User = db.model("User", UserSchema);
+export const User = db.model("User", UserScheme);
--- settings.js
+++ settings.js
@@ -1,12 +1,12 @@
 export default (() => {
-	/**
-	 *
-	 * @param value
-	 * @returns {boolean}
-	 */
-	Array.prototype.uniquePush = function (value) {
-		const inc = this.includes(value)
-		if (!inc) this.push(value)
-		return !inc
-	}
-})()
+  /**
+   *
+   * @param value
+   * @returns {boolean}
+   */
+  Array.prototype.uniquePush = function (value) {
+    const inc = this.includes(value);
+    if (!inc) this.push(value);
+    return !inc;
+  };
+})();
