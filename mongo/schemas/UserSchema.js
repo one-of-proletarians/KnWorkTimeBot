9026f663c235a1d5836aa3b4b706d77ab48fb32d
Vitalii <0x1d46@gmail.com>
2022-03-17 17:45:26 +0100

    Added filter records method

--- mongo/schemas/UserSchema.js
+++ mongo/schemas/UserSchema.js
@@ -20,7 +20,20 @@ export const UserScheme = new mongoose.Schema({
   ],
 });
 
+/**
+ *
+ * @param {UserScheme._id} id
+ * @returns {Promise<[{date: String | StringConstructor, value: {amount: Number | NumberConstructor, start: Number | NumberConstructor, end: Number | NumberConstructor}}]|[]|*>}
+ */
 UserScheme.statics.getRecords = async function (id) {
   const user = await this.findById(id);
   return user.records;
 };
+
+/**
+ *
+ * @param {[UserRecord]}records
+ * @param {RegExp}regex
+ */
+UserScheme.statics.recordsByRegex = (records, regex) =>
+  records.filter(record => regex.test(record.date));
