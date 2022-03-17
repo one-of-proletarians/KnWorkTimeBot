e9ec6e32619470c6e86ef0306a0e3df1977fd33f
Vitalii <0x1d46@gmail.com>
2022-03-17 08:49:03 +0100

    Опечатка

--- mongo/mongo.js
+++ mongo/mongo.js
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
 import { isDev } from "../assets/helpers.js";
-import UserShema from "./schemas/UserSchema.js";
+import UserSchema from "./schemas/UserSchema.js";
 
 const dev = isDev();
 const mongoConnect = process.env[dev ? "DEV_MONGO" : "MONGO"];
 
 const db = mongoose.createConnection(mongoConnect);
 
-export const User = db.model("User", UserShema);
+export const User = db.model("User", UserSchema);
